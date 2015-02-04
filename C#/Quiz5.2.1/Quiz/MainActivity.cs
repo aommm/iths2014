e85fb24a9be9895401dfcaabbbdcfc6877ba78ce
Jonathan <jonathan.orro@gmail.com>
2015-02-04 21:42:48 +0100

    Added quiz-demo

---
+++ C#/Quiz5.2.1/Quiz/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Quiz
+{
+    [Activity(Label = "Quiz", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private Button questionButton, infoButton, newQuestionButton;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.main_activity);
+
+            questionButton = FindViewById<Button>(Resource.Id.quest);
+            infoButton = FindViewById<Button>(Resource.Id.info);
+            newQuestionButton = FindViewById<Button>(Resource.Id.new_quest);
+
+            questionButton.Click += delegate
+            {
+                Intent i = new Intent(this, typeof(QuestionActivity));
+                StartActivity(i);
+            };
+
+            newQuestionButton.Click += delegate
+            {
+                Intent i = new Intent(this, typeof(NewQuestionActivity));
+                StartActivity(i);
+            };
+        }
+    }
+}
+
---
+++ C#/Quiz5.2.1/Quiz/QuestionManager.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using SQLite;
+
+namespace Quiz
+{
+    public class QuestionManager
+    {
+        private static QuestionManager instance;
+        public static QuestionManager Instance
+        {
+            get
+            {
+                instance = instance ?? new QuestionManager();
+                return instance; 
+            }
+        }
+
+        private string dbPath;
+        private Random r;
+        public Question RandomQuestion
+        {
+            get
+            {
+                SQLiteConnection db = new SQLiteConnection(dbPath);
+                List<Question> questions = db.Table<Question>().ToList();
+                
+                return questions[r.Next(questions.Count())];
+            }
+        }
+
+        public void AddQuestion(Question q)
+        {
+            SQLiteConnection db = new SQLiteConnection(dbPath);
+            db.Insert(q);
+            db.Close();
+        }
+        private QuestionManager()
+        {
+            dbPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal) + @"/database.db";
+            SQLiteConnection db = new SQLiteConnection(dbPath);
+            db.CreateTable<Question>();
+
+            r = new Random();
+
+            db.DeleteAll<Question>();
+
+            if (db.Table<Question>().Count() == 0)
+            {
+                db.Insert(new Question()
+                {
+                    Quest = "Vad heter jag?",
+                    Answer1 = "Jonathan",
+                    Answer2 = "Niklas",
+                    Answer3 = "Bonathan",
+                    Answer4 = "Biklas",
+                    RightAnswer = 1
+                });
+                db.Insert(new Question()
+                {
+                    Quest = "Vilken färg är blå?",
+                    Answer1 = "Röd",
+                    Answer2 = "Grön",
+                    Answer3 = "Brun",
+                    Answer4 = "Svart",
+                    RightAnswer = 3
+                });
+            }
+        }
+    }
+}

7982d6dc07ba0053d2018912ec15f6097a6b6972
aommm <niklas@niklaslogren.com>
2015-02-20 13:15:04 +0100

    Add old C# demos

---
+++ C#/LambdaFun/LambdaFun/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaFun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ss = new string[] { "khej", "bobobo", "adidjskdlfijf" };
+            // returnerar “adidjskdlfijf” (störst längd)
+            Console.WriteLine(MaxString(ss, s => s.Length));
+            // returnerar “khej” (störst första bokstav)
+            Console.WriteLine(MaxString(ss, s => -s[0])); 
+
+        }
+
+        public delegate int StringToInt(string s);
+
+        static string MaxString(string[] ss, StringToInt fn)
+        {
+            string biggestString = null;
+            int biggestValue = 0;
+            bool firstRun = true;
+
+            for (int i = 0; i < ss.Length; i++)
+            {
+                int currentValue = fn(ss[i]);
+                if (firstRun || (biggestValue < currentValue))
+                {
+                    biggestValue = currentValue;
+                    biggestString = ss[i];
+                    firstRun = false;
+                }
+            }
+            return biggestString;
+        }
+
+    }
+}
---
+++ C#/f2.1.1/f2.1.1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace f2._1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        string Name { get; set; }
+    }
+}

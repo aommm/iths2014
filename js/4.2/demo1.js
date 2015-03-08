affed1e837b89ea56ba166f189143cbef2e86a42
Niklas Logren <niklas@niklaslogren.com>
2015-03-08 13:24:01 +0100

    added new 4.2 demo

--- js/4.2/demo1.js
+++ js/4.2/demo1.js
@@ -1,66 +1,42 @@
 $(document).ready(function() {
 
-var items = [{name: 'Niklas', age: 22, interests: ['programming', 'running', 'books', 'bananas'], points: 900},
-{name: 'Christer', age: 37, interests: ['geocaching'], points: 30},
-{name: 'Ulla', age: 11, interests: ['electricity', 'lights', 'cats'], points: 9512}]
 
-var settings = {
-  sortBy: undefined,
-  sortAscending: true
-}
+var inputs = $("form input").toArray();
+var errorSpan = $("#error");
 
-function renderPage() {
-  console.log("rendering");
-  var html = "<table border='1'><tr><th><a href='#' id='name'>Namn</a></th><th><a  href='#' id='age'>Ålder</a></th><th><a href='#' id='interests'>Intressen</a></th><th><a href='#' id='points'>Poäng</a></th></tr>"
-  items.forEach(function(item) {
-    html += renderItem(item);
-  })
-  html += "</table>"
-  $("#content").html(html); 
+$("form").submit(function(e) {
 
-  // Re-attach events
-  $("#rerender").click(renderPage);
-  $("#name").click(_.partial(sort, 'name'));
-  $("#age").click(_.partial(sort, 'age'));
-  $("#interests").click(_.partial(sort, 'interests'));
-  $("#points").click(_.partial(sort, 'points'));
-}
-
-function renderItem(item) {
-  var interests = _.reduce(item.interests, function(str,i) {return (str?str+", ":"")+i}, "");
-  return "<tr><td>"+item.name+"</td><td>"+item.age+"</td><td>"+interests+"</td><td>"+item.points+"</td></tr>"
-}
-
-function sort(name) {
-  console.log("SORTING!");
-  // Sort items by the given property name
-  items = _.sortBy(items, _.property(name));
-
-  // Change sorting settings (and possibly swap asc/desc)
-  if (settings.sortBy === name) {
-    if (settings.sortAscending) {
-      settings.sortAscending = false;
-      items = _(items).reverse().value();
-    } else {
-      settings.sortAscending = true;
+  // Function which checks if a DOM input element is empty or not
+  var nonEmpty = function(input) {return $(input).val();}
+  
+  // All elements are okay:
+  if (_(inputs).every(nonEmpty)) {
+    // Allow through
+    errorSpan.hide();
+    return true;
+  }
+  // Some elements are not okay:
+  else {
+    // Function which creates error message
+    var buildError = function(msg, input) {
+      // New message is total message + current field name, if invalid
+      var fieldName = $(input).attr('name')
+       ,  errorMsg  = msg + (nonEmpty(input)? "" : (fieldName + ", "));
+      return errorMsg;
     }
-  } else {
-    settings.sortBy = name;
+    // Generate error message
+    var totalError = _(inputs).reduce(buildError, "Error in fields: ");
+    console.log(totalError);
+    totalError = totalError.slice(0, totalError.length -2);
+    console.log(totalError);
+    // Show error message
+    errorSpan.text(totalError);
+    errorSpan.show();
+    // Do not allow through
+    return false;
   }
 
-  // Re-render page
-  renderPage();
-}
-
-
-renderPage();
-
+  
 });
 
-// TODO:
-// Add fold which sums all ages
-// Generate a list of all interests
-// Add searching
-// ...
-
-
+});

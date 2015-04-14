0754811fa2aee30cde3d5c38aa6d39d77e5e6a3f
Niklas Logren <niklas@niklaslogren.com>
2015-04-14 20:24:55 +0200

    Updated 6.1 demo

--- js/6.1/demo1.js
+++ js/6.1/demo1.js
@@ -17,18 +17,30 @@ $(document).ready(function() {
   // ??? Ser fortfarande ut som att y ligger i c1...
   // ... Men det gör den inte!
 
-  console.log(c1.hasOwnProperty("x"));
-  console.log(c1.hasOwnProperty("hello"));
-  console.log(c1.hasOwnProperty("y"));
+  console.log(c1.hasOwnProperty("x")); // true
+  console.log(c1.hasOwnProperty("hello")); // true
+  console.log(c1.hasOwnProperty("y")); // false
 
 
   // JavaScript letar först PÅ objektet,
   // och SEDAN i prototypen:
+c1.y = 159;
+console.log(c1.y); // 159
+console.log(c1.hasOwnProperty("y")); // true
 
   // TODO overrrida y på instansen
   // visa att prototypen inte konsulteras
 
-  // TODO skapa ny klass, overrida y i konstruktorn,
-  // men ha i prototypen. visa att prototypen inte visas
+  // Att lägga en egenskap direkt på objektet kan såklart göras 
+  // direkt i konstruktorn också:
+
+  MySndComponent = function() {
+    this.thing = "blobb";
+  }
+  MySndComponent.prototype.thing = "hej";
+  var c2 = new MySndComponent();
+
+  console.log(c2.thing)
+  console.log(c2.hasOwnProperty("thing"));
 
 });

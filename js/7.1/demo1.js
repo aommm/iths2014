cff285f082931c568aa364cac3ea5b3e867e8c86
aommm <niklas@niklaslogren.com>
2015-04-15 09:02:57 +0200

    Added angular 7.1.1

---
+++ js/7.1/demo1.js
@@ -0,0 +1,11 @@
+// Create our app module 'demo1'
+var app = angular.module('demo1', []);
+
+// Create a controller 'Demo1Ctrl' in the module,
+// which has access to $scope, i.e. the model
+app.controller('Demo1Ctrl', ['$scope', function($scope) {
+  // This function is run when controller is instantiated
+
+  // Store something in the model:
+  $scope.x = 350;
+}]);
